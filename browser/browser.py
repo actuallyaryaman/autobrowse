91d09e69e9187e6b32dd5aea906aabf709e0865d
actuallyaryaman <aryaman4real@outlook.com>
2023-02-26 23:22:59 +0530

    shifted wordfile to an array to reduce number of files
    Co-authored-by: baked-patatoes <70744212+baked-potatoes@users.noreply.github.com>

--- browser/browser.py
+++ browser/browser.py
@@ -2,10 +2,10 @@ import webbrowser
 import random
 import time
 
+data = ["nor", "norm", "normal", "normalcy", "normalcy's", "normality", "normality's", "normalization", "normalization's", "normalize", "normalized", "normalizes", "normalizing", "normally", "normal's", "normative", "norm's", "norms", "north", "northbound", "northeast", "northeaster", "northeasterly", "northeastern", "northeaster's", "northeasters", "northeast's", "northeastward", "northerlies", "northerly", "northerly's", "northern", "northerner", "northerner's", "northerners", "northernmost", "north's", "northward", "northwards", "northwest", "northwesterly", "northwestern", "northwest's", "northwestward", "no's", "nos", "nose", "nosebleed", "nosebleed's", "nosebleeds", "nosed", "nosedive", "nosedived", "nosedive's", "nosedives", "nosediving", "nosedove", "nosegay", "nosegay's", "nosegays", "nose's", "noses", "nosey", "nosh", "noshed", "noshes", "noshing", "nosh's", "nosier", "nosiest", "nosiness", "nosiness's", "nosing", "nostalgia", "nostalgia's", "nostalgic", "nostalgically", "nostril", "nostril's", "nostrils", "nostrum", "nostrum's", "nostrums", "nosy", "not", "notable", "notable's", "notables", "notably", "notaries"]
+
 a=input("is this unix(1) or windows(2)?: ")
 if (a=="1"):
-    with open("browser/words", 'r', encoding='utf-8') as f:
-        data = f.read().splitlines()
     n = 10
     while n!= 0:
         w = data[random.randint(0, len(data))]
@@ -13,8 +13,6 @@ if (a=="1"):
         n -= 1
         time.sleep(2)
 elif (a=="2"):
-    with open("words", 'r', encoding='utf-8') as f:
-        data = f.read().splitlines()
     n = 10
     while n!= 0:
         w = data[random.randint(0, len(data))]
