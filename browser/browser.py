a6b6215c42fed873a69896d824d5d29beb9d5460
actuallyaryaman <aryaman4real@outlook.com>
2023-02-26 22:34:36 +0530

    major improvements

--- browser/browser.py
+++ browser/browser.py
@@ -2,11 +2,24 @@ import webbrowser
 import random
 import time
 
-with open("words", 'r', encoding='utf-8') as f:
-    data = f.read().splitlines()
-n = 10
-while n!= 0:
-    w = data[random.randint(0, len(data))]
-    webbrowser.open_new_tab("https://www.bing.com/search?q="+w)
-    n -= 1
-    time.sleep(2)
+a=input("is this unix(1) or windows(2)?: ")
+if (a=="1"):
+    with open("browser/words", 'r', encoding='utf-8') as f:
+        data = f.read().splitlines()
+    n = 10
+    while n!= 0:
+        w = data[random.randint(0, len(data))]
+        webbrowser.open_new_tab("https://www.bing.com/search?q="+w)
+        n -= 1
+        time.sleep(2)
+elif (a=="2"):
+    with open("browser\words", 'r', encoding='utf-8') as f:
+        data = f.read().splitlines()
+    n = 10
+    while n!= 0:
+        w = data[random.randint(0, len(data))]
+        webbrowser.get("C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe").open_new_tab("https://www.bing.com/search?q="+w)
+        n -= 1
+        time.sleep(2)
+else:
+    print("we hate mac")
